cf3bb850a7986a3f5ac5b2611aa346ef9616fe08
Deap Daru <deapdaru@gmail.com>
2021-07-26 19:39:00 +0530

    Initial Gatsby setup with Unicode logo and icon.

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	siteMetadata: {
+		siteUrl: "https://www.djunicode.in",
+		title: "D. J. Unicode",
+		description:
+			"Official site for Unicode, a student coding club of Dwarkadas J. Sanghvi College of Engineering.",
+	},
+	plugins: [
+		"gatsby-plugin-image",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sitemap",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/images/unicode-icon.svg",
+			},
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+	],
+};
