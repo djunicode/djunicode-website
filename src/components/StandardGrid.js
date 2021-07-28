3a15506ba00023616b76dc4dd4286fbc92be9eb8
Akshath <akshathmahajan13@gmail.com>
2021-07-29 01:40:32 +0530

    Added quote card component
    Renamed TechStackGrid to StandardGrid
    
    QuoteCard component created with default image - avatar.png
    The component will be used with a grid container - StandardGrid
    as a container

--- src/components/StandardGrid.js
+++ src/components/StandardGrid.js
@@ -3,21 +3,18 @@ import { Grid } from "@material-ui/core";
 
 /*
     This is just a simple placeholder so that we know what type of grid we're using
-    for the tech stack grid. 
+    for the tech stack grid and Quote Card grid. 
     This component will most likely be deleted and directly incorporated in the page. 
-    
-    It can also be reimplemented to simply recieve an array of tech stacks and their 
-    logos and render TechStack component for each item in the array
-    which will make imports on the index page cleaner.
 */
 
-function TechStackGrid({children}) {
+function StandardGrid({children}) {
     return (
         <Grid container justifyContent="space-evenly">
             {children}
             {/* <TechStack imgUrl={logo} title="Django"/> */}
+            {/* <QuoteCard quote="Lorem Ipsum..." name="Joe" avatar={url} */}
         </Grid>
     )
 }
 
-export default TechStackGrid
+export default StandardGrid
