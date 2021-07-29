2c35d7d8380b21bcb52543642aff1209772c85b5
vedant-5 <gandhivedant01@gmail.com>
2021-07-29 13:56:07 +0530

    feat: landing page text

---
+++ src/pages/events/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import GridItem from "../../components/GridItem";
+import Layout from "../../components/Layout";
+
+const data = [
+	{
+		key: "1",
+		title: "Project 1",
+		description:
+			"Voluptate nisi ut pariatur proident qui quis ipsum cillum exercitation consequat occaecat officia deserunt minim.",
+	},
+	{
+		key: "2",
+		title: "Project 2",
+		description:
+			"Voluptate nisi ut pariatur proident qui quis ipsum cillum exercitation consequat occaecat officia deserunt minim.",
+	},
+	{
+		key: "3",
+		title: "Project 3",
+		description:
+			"Voluptate nisi ut pariatur proident qui quis ipsum cillum exercitation consequat occaecat officia deserunt minim.",
+	},
+	{
+		key: "4",
+		title: "Project 4",
+		description:
+			"Voluptate nisi ut pariatur proident qui quis ipsum cillum exercitation consequat occaecat officia deserunt minim.",
+	},
+	{
+		key: "5",
+		title: "Project 5",
+		description:
+			"Voluptate nisi ut pariatur proident qui quis ipsum cillum exercitation consequat occaecat officia deserunt minim.",
+	},
+	{
+		key: "6",
+		title: "Project 6",
+		description:
+			"Voluptate nisi ut pariatur proident qui quis ipsum cillum exercitation consequat occaecat officia deserunt minim.",
+	},
+];
+
+export default function Project() {
+	return (
+		<Layout>
+			<GridItem data={data} />
+		</Layout>
+	);
+}
