7815628ee8b9de3c79cbb4fa818d98a0c4317e0f
Deap Daru <deapdaru@gmail.com>
2021-07-29 15:43:16 +0530

    Styling files restructure.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,25 +1,22 @@
-import { Grid } from '@material-ui/core'
-import React from 'react'
-import * as styles from '../styles/quotecard.module.scss'
-import defaultAvatar from '../images/avatar.png'
+import { Grid } from "@material-ui/core";
+import React from "react";
+import * as styles from "../styles/components/quotecard.module.scss";
+import defaultAvatar from "../images/avatar.png";
 
-
-function QuoteCard({quote, name, avatar}) {
-    return (
-        <Grid item xs={8} sm={5} lg={4}>
-            <div className={styles.card}>
-                <div className={styles.cardLayer}>
-                    <div className={styles.imgContainer}>
-                        <img src={avatar || defaultAvatar} alt="Avatar" />
-                    </div>
-                    <h3 className={styles.header}>{name}</h3>
-                    <div className={styles.content}>
-                        {quote}
-                    </div>
-                </div>
-            </div>
-        </Grid>
-    )
+function QuoteCard({ quote, name, avatar }) {
+	return (
+		<Grid item xs={8} sm={5} lg={4}>
+			<div className={styles.card}>
+				<div className={styles.cardLayer}>
+					<div className={styles.imgContainer}>
+						<img src={avatar || defaultAvatar} alt="Avatar" />
+					</div>
+					<h3 className={styles.header}>{name}</h3>
+					<div className={styles.content}>{quote}</div>
+				</div>
+			</div>
+		</Grid>
+	);
 }
 
-export default QuoteCard
+export default QuoteCard;
--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Grid } from '@material-ui/core'
-import * as styles from '../styles/techstack.module.scss'
+import React from "react";
+import { Grid } from "@material-ui/core";
+import * as styles from "../styles/components/techstack.module.scss";
 
 /*
 ____________________
@@ -15,17 +15,17 @@ ____________________
 
 */
 
-function TechStack({imgUrl, title}) {
-    return (
-        <Grid item container xs={5} sm={3} className={styles.container}>
-            <Grid item xs={12}>
-                <img src={imgUrl} alt={title+" logo"} className={styles.img}/>
-            </Grid>
-            <Grid item xs={12}>
-                <h4> {title} </h4>
-            </Grid>
-        </Grid>
-    )
+function TechStack({ imgUrl, title }) {
+	return (
+		<Grid item container xs={5} sm={3} className={styles.container}>
+			<Grid item xs={12}>
+				<img src={imgUrl} alt={title + " logo"} className={styles.img} />
+			</Grid>
+			<Grid item xs={12}>
+				<h4> {title} </h4>
+			</Grid>
+		</Grid>
+	);
 }
 
-export default TechStack
+export default TechStack;
