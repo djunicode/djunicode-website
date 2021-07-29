eeba40926ccd78e6a34b64b6ad818b3920ff51f6
Deap Daru <deapdaru@gmail.com>
2021-07-29 21:32:29 +0530

    Material UI custom theme synced with Sass variables.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,21 @@
 import React from "react";
 
+// Material UI
+import { ThemeProvider } from "@material-ui/core/styles";
+
 // Components
 import NavBar from "./NavBar";
 
 // Global Styles
 import "../styles/global.scss";
+import { theme } from "../theme";
 
 function Layout({ children }) {
 	return (
-		<React.Fragment>
+		<ThemeProvider theme={theme}>
 			<NavBar />
 			<div className="fixedcontainer">{children}</div>
-		</React.Fragment>
+		</ThemeProvider>
 	);
 }
 
