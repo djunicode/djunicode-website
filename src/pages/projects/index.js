7d8ed30eacc07cc87566de3c3183c2447427d717
Deap Daru <deapdaru@gmail.com>
2021-08-01 18:03:26 +0530

    Querying projects data on Projects page.

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,46 @@
 import React from "react";
+
+import { graphql } from "gatsby";
+
 import GridItem from "../../components/GridItem";
 import Layout from "../../components/Layout";
 import Seo from "../../components/SEO";
-import data from "../../data/projects/projects.json";
 
-export default function Project() {
+export default function Project({ data }) {
 	return (
 		<Layout>
 			<Seo title="Projects" />
-			<GridItem data={data} />
+			<GridItem data={data.allProjectsJson.nodes} />
 		</Layout>
 	);
 }
+
+export const query = graphql`
+	query ProjectDetails {
+		allProjectsJson {
+			nodes {
+				images
+				title
+				stack
+				links
+				description
+				contributors {
+					SEmentees {
+						GitHub
+						LinkedIn
+						desc
+						name
+						profile_img
+					}
+					TEmentors {
+						GitHub
+						LinkedIn
+						desc
+						name
+						profile_img
+					}
+				}
+			}
+		}
+	}
+`;
