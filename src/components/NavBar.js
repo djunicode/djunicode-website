8a9c44066bccf0142e3d9a6d3fadfd76c1311496
Deap Daru <deapdaru@gmail.com>
2021-08-20 15:59:45 +0530

    NavBar styling changes.
    Unicode logo size increase and padding addition to increase navbar
    height. On scroll navbar will have a bottom border.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,6 +9,7 @@ import IconButton from "@material-ui/core/IconButton";
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 
 // Material UI Icons
 import MenuIcon from "@material-ui/icons/Menu";
@@ -25,7 +26,20 @@ import * as styles from "../styles/components/navbar.module.scss";
 // Constants
 import { navbarNavigations } from "../constants";
 
-function NavBar() {
+function ElevationScroll(props) {
+	const { children } = props;
+
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0,
+	});
+
+	return React.cloneElement(children, {
+		elevation: trigger ? 1 : 0,
+	});
+}
+
+function NavBar(props) {
 	const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 	const navbarBreakpoint = useMediaQuery("(min-width:657px)");
 
@@ -36,53 +50,59 @@ function NavBar() {
 	};
 
 	return (
-		<AppBar color="inherit" position="sticky" className={styles?.navbar}>
-			<Toolbar className={styles?.navbarContainer + " fixedcontainer"}>
-				<Link to="/">
-					<img className={styles?.navbarLogo} src={logoDataUri} alt="Unicode" />
-				</Link>
-				<div className={styles?.navbarNavigations}>
-					{navbarBreakpoint ? (
-						<React.Fragment>
-							{navbarNavigations.map((page, index) => (
-								<Link
-									to={page.url}
-									key={index}
-									className={styles?.navbarButtons}
-								>
-									<Button tabIndex={-1} size="large" color="inherit">
-										{page.name}
-									</Button>
-								</Link>
-							))}
-						</React.Fragment>
-					) : (
-						<IconButton onClick={toggleDrawer}>
-							<MenuIcon />
-						</IconButton>
-					)}
-				</div>
-			</Toolbar>
+		<ElevationScroll {...props}>
+			<AppBar color="inherit" position="sticky" className={styles?.navbar}>
+				<Toolbar className={styles?.navbarContainer + " fixedcontainer"}>
+					<Link to="/">
+						<img
+							className={styles?.navbarLogo}
+							src={logoDataUri}
+							alt="Unicode"
+						/>
+					</Link>
+					<div className={styles?.navbarNavigations}>
+						{navbarBreakpoint ? (
+							<React.Fragment>
+								{navbarNavigations.map((page, index) => (
+									<Link
+										to={page.url}
+										key={index}
+										className={styles?.navbarButtons}
+									>
+										<Button tabIndex={-1} size="large" color="inherit">
+											{page.name}
+										</Button>
+									</Link>
+								))}
+							</React.Fragment>
+						) : (
+							<IconButton onClick={toggleDrawer}>
+								<MenuIcon />
+							</IconButton>
+						)}
+					</div>
+				</Toolbar>
 
-			<SwipeableDrawer
-				anchor="top"
-				open={!navbarBreakpoint && openDrawer}
-				onClose={toggleDrawer}
-				onOpen={toggleDrawer}
-				disableBackdropTransition={!iOS}
-				disableDiscovery={iOS}
-			>
-				<List className={styles?.navbarNavigations}>
-					{navbarNavigations.map((page, index) => (
-						<Link to={page.url} key={index}>
-							<ListItem button className={styles?.navbarListItem}>
-								{page.name}
-							</ListItem>
-						</Link>
-					))}
-				</List>
-			</SwipeableDrawer>
-		</AppBar>
+				<SwipeableDrawer
+					anchor="top"
+					open={!navbarBreakpoint && openDrawer}
+					onClose={toggleDrawer}
+					onOpen={toggleDrawer}
+					disableBackdropTransition={!iOS}
+					disableDiscovery={iOS}
+				>
+					<List className={styles?.navbarNavigations}>
+						{navbarNavigations.map((page, index) => (
+							<Link to={page.url} key={index}>
+								<ListItem button className={styles?.navbarListItem}>
+									{page.name}
+								</ListItem>
+							</Link>
+						))}
+					</List>
+				</SwipeableDrawer>
+			</AppBar>
+		</ElevationScroll>
 	);
 }
 
