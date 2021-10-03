17c440d05327fe82ac91c787387d73a91f79a9fc
Deap Daru <deapdaru@gmail.com>
2021-10-03 13:18:37 +0530

    Minor build bug fix.

--- src/components/AvatarHelper.js
+++ src/components/AvatarHelper.js
@@ -7,7 +7,7 @@ function AvatarHelper({header, data}) {
         <Grid item xs={12}><h3>{header}</h3></Grid>
         <Grid item container xs={12} justifyContent="flex-start">
             {
-                data.map((item, idx) => <TechStack key={idx} xs={4} sm={3} md={2} img={item.profile_pic.childImageSharp.gatsbyImageData} title={item.name}/>)
+                data.map((item, idx) => <TechStack key={idx} xs={4} sm={3} md={2} img={item.profile_pic?.childImageSharp?.gatsbyImageData} title={item.name}/>)
             }
         </Grid>
     </>)
