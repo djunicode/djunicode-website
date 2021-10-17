cb99cb46da9d1867f5de4f709ef546135f5e7c29
Akshath <akshathmahajan13@gmail.com>
2021-10-17 12:39:56 +0530

    Fixed overlay centring in featured projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,10 +25,9 @@ const IndexPage = ({ data }) => {
 					<Link to={`/projects/${project.slug}`}>
 						<div className={styles.overlay}>
 							<h3>{project.title}</h3>
-							<p>Built with - </p>
-							{project.stack.map(
-								(item, idx)=><p key={idx} className={styles.overlayText}>{idx+1}.{item}</p>
-							)}
+							<p className={styles.overlayText}>
+								{project.stack.join(', ')}
+							</p>
 						</div>
 						<GatsbyImage
 							key={index}
